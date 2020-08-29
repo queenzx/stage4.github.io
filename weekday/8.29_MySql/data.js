80af6afb71289dad3b932c363d6979470ca3b153
apple <123456789@qq.com>
2020-08-29 16:56:11 +0800

    demo

---
+++ weekday/8.29_MySql/data.js
@@ -0,0 +1,33 @@
+let mysql = require('mysql');
+
+let connect = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '123',
+    database : 'web'
+});
+connect.connect();
+
+let sql_r = "select * from stus"
+connect.query(sql_r,(err,result,fields)=>{
+    console.log(err);
+    result.forEach(val=>{
+        let id = val.id;
+        let name = val.name;
+        let b = val.birth;
+        let str = `${b.getFullYear()}-${b.getMonth()+1}-${b.getDate()}`
+        console.log(`id: ${id}, name: ${name}, birth: ${str}`);
+        // console.log(`id:${val.id},name:${val.name},birth:${val.birth}`)
+    })
+    connect.end();
+});
+
+/* let sql_a = `insert into stus values (101,"zhaoliu",?)`;
+connect.query({
+    sql:sql_a,
+    values:[new Date()]
+},(err,result,fileds)=>{
+    console.log(err)
+    console.log(result)
+    connect.end();
+}) */
